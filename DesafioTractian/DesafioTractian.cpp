ad7d80d36e55f99b9ec0ae405f2a1f24477ffc0a
luccaki <lucca.ianaguivara@gmail.com>
2020-10-13 03:06:08 -0300

    done?

--- DesafioTractian/DesafioTractian.cpp
+++ DesafioTractian/DesafioTractian.cpp
@@ -9,9 +9,16 @@ using namespace std;
 
 #define N 5000
 
-void fft(double* in, fftw_complex* out, int sum)
+/*void fft(double* in, fftw_complex* out, int sum)
 {
 	fftw_plan plan = fftw_plan_dft_r2c_1d(sum, in, out, FFTW_ESTIMATE);
+	fftw_execute(plan);
+	fftw_destroy_plan(plan);
+	fftw_cleanup();
+}*/
+void fft(double* in, double* out, int sum)
+{
+	fftw_plan plan = fftw_plan_r2r_1d(sum, in, out, FFTW_REDFT00, FFTW_ESTIMATE);
 	fftw_execute(plan);
 	fftw_destroy_plan(plan);
 	fftw_cleanup();
@@ -24,12 +31,18 @@ int main() {
 	x = (double*)malloc(sizeof(double) * N);
 	y = (double*)malloc(sizeof(double) * N);
 	z = (double*)malloc(sizeof(double) * N);
-	fftw_complex* outx;
+	double* outx;
+	double* outy;
+	double* outz;
+	outx = (double*)malloc(sizeof(double) * N);
+	outy = (double*)malloc(sizeof(double) * N);
+	outz = (double*)malloc(sizeof(double) * N);
+	/*fftw_complex* outx;
 	outx = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * N);
 	fftw_complex* outy;
 	outy = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * N);
 	fftw_complex* outz;
-	outz = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * N);
+	outz = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * N);*/
 	ifstream myfile("1602245833-2715-NAO7856.txt");
 	int sum = 0;
 	if (myfile.is_open()) {
@@ -53,13 +66,20 @@ int main() {
 	fft(y, outy, sum);
 	fft(z, outz, sum);
 	ofstream ofs("output.txt", ofstream::out);
-	double freq = 0.736826270;
-	for (int i = 1; i < 2069; i++) {
+	double freq = 0.368324125;
+	/*for (int i = 1; i < 2069; i++) {
 		ofs << sqrt((outx[i][0] * outx[i][0]) + (outx[i][1] * outx[i][1])) << ","
 			<< sqrt((outy[i][0] * outy[i][0]) + (outy[i][1] * outy[i][1])) << "," 
 			<< sqrt((outz[i][0] * outz[i][0]) + (outz[i][1] * outz[i][1])) << "," 
 			<< freq << endl;
 		freq += 0.736826270;
+	}*/
+	for (int i = 1; i < sum; i++) {
+		ofs << sqrt((outx[i] * outx[i]) + (outx[i] * outx[i])) << ","
+			<< sqrt((outy[i] * outy[i]) + (outy[i] * outy[i])) << ","
+			<< sqrt((outz[i] * outz[i]) + (outz[i] * outz[i])) << ","
+			<< freq << endl;
+		freq += 0.368324125;
 	}
 	return 0;
 }
